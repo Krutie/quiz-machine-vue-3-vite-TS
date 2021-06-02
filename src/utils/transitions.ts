d9d5660eacbf46ea7b740fe29c8ea8b3db02f35f
Krutie Patel <krutie.patel@gmail.com>
2021-06-03 09:56:27 +1000

    transition return type added

--- src/utils/transitions.ts
+++ src/utils/transitions.ts
@@ -1,7 +1,7 @@
 // GSAP
 import gsap from "gsap";
 
-export const shake = (element: string) => {
+export const shake = (element: string): void => {
   // Shake the box when the answer is invalid
   gsap.to(element, { duration: 0.1, x: 100 })
   gsap.to(element, {
